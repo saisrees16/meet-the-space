f960f7b0f88547cdb7ae32c5911b7a8161841255
Sai Sree <sai@gmail.com>
2025-02-19 15:19:38 +0530

    arpage

--- src/Arpage.jsx
+++ src/Arpage.jsx
@@ -10,7 +10,7 @@ const Arpage = () => {
                 cameraRig.setAttribute("position", "0 50 0");
                 cameraRig.setAttribute("rotation", "-90 0 0");
             } else {
-                cameraRig.setAttribute("position", "0 15 40");
+                cameraRig.setAttribute("position", "0 0 30"); // Move closer from 40 to 20
                 cameraRig.setAttribute("rotation", "0 0 0");
             }
         };
@@ -58,19 +58,55 @@ const Arpage = () => {
                 </figure>
             </div>
             <a-scene vr-mode-ui="enabled: false">
-                <a-entity id="cameraRig" position="0 5 40">
+                <a-entity id="cameraRig" position="0 0 30">
                     <a-camera id="mainCamera" wasd-controls="acceleration: 50" zoom="1"></a-camera>
                 </a-entity>
+
+                {/* Sky background */}
                 <a-sky material="src: https://cdn.glitch.com/4cfbcd0a-1382-44f1-b55c-1f16f20ea425%2FSolarsystemscope_texture_8k_stars_milky_way.jpg?v=1624110041528"></a-sky>
+
+                {/* Sun */}
                 <a-sphere position="0 0 0" radius="5.25" material="src: https://cdn.glitch.com/4cfbcd0a-1382-44f1-b55c-1f16f20ea425%2Fmaterial_1_baseColor.jpg?v=1624106708644" shadow></a-sphere>
-                <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 8700; easing: linear">
-                    <a-sphere position="7 0.5 0" radius="0.5" material="color: gray"></a-sphere>
+
+                {/* Mercury */}
+                <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 8800; easing: linear">
+                    <a-sphere position="4 0 0" radius="0.38" material="color: #A0522D"></a-sphere>
                 </a-entity>
-                <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 22000; easing: linear">
-                    <a-sphere position="10 1 0" radius="1.1" material="color: yellow"></a-sphere>
+
+                {/* Venus */}
+                <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 22500; easing: linear">
+                    <a-sphere position="6 0 0" radius="0.95" material="color: #DEB887"></a-sphere>
                 </a-entity>
-                <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 36000; easing: linear">
-                    <a-sphere position="14 0 0" radius="1.25" material="src: https://cdn.glitch.com/4cfbcd0a-1382-44f1-b55c-1f16f20ea425%2F8081_earthmap2k.jpg?v=1624107209665"></a-sphere>
+
+                {/* Earth */}
+                <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 36500; easing: linear">
+                    <a-sphere position="8 0 0" radius="1" material="src: https://cdn.glitch.com/4cfbcd0a-1382-44f1-b55c-1f16f20ea425%2F8081_earthmap2k.jpg?v=1624107209665"></a-sphere>
+                </a-entity>
+
+                {/* Mars */}
+                <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 68700; easing: linear">
+                    <a-sphere position="10 0 0" radius="0.53" material="color: #CD5C5C"></a-sphere>
+                </a-entity>
+
+                {/* Jupiter */}
+                <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 433200; easing: linear">
+                    <a-sphere position="13 0 0" radius="2.5" material="color: #DEB887"></a-sphere>
+                </a-entity>
+
+                {/* Saturn */}
+                <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 1075200; easing: linear">
+                    <a-sphere position="17 0 0" radius="2.1" material="color: #F4A460"></a-sphere>
+                    <a-torus position="17 0 0" radius="3" radius-tubular="0.1" rotation="90 0 0" material="color: #DAA520"></a-torus>
+                </a-entity>
+
+                {/* Uranus */}
+                <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 3066000; easing: linear">
+                    <a-sphere position="20 0 0" radius="1.8" material="color: #87CEEB"></a-sphere>
+                </a-entity>
+
+                {/* Neptune */}
+                <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 6019200; easing: linear">
+                    <a-sphere position="23 0 0" radius="1.7" material="color: #4169E1"></a-sphere>
                 </a-entity>
             </a-scene>
         </div>
